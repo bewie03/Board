4a8167474ee0042cbc5d033a76af85acb875130e
bewie03 <your-email@example.com>
2025-08-29 11:43:06 +1000

    feat: general enhancements

--- boneboard/src/pages/AdminPanel.tsx
+++ boneboard/src/pages/AdminPanel.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { FaBriefcase, FaChartBar, FaExclamationTriangle, FaArchive, FaClock, FaTrash, FaShieldAlt, FaDollarSign, FaTimes, FaGlobe, FaExternalLinkAlt, FaEnvelope, FaCalendarAlt, FaMapMarkerAlt, FaPlay, FaCoins, FaInfoCircle, FaTrashAlt } from 'react-icons/fa';
+import { FaBriefcase, FaChartBar, FaExclamationTriangle, FaClock, FaTrash, FaShieldAlt, FaDollarSign, FaTimes, FaGlobe, FaExternalLinkAlt, FaEnvelope, FaCalendarAlt, FaMapMarkerAlt, FaPlay, FaCoins, FaInfoCircle, FaTrashAlt } from 'react-icons/fa';
 import { useWallet } from '../contexts/WalletContext';
 import PageTransition from '../components/PageTransition';
 
