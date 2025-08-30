43d7fca083db68b7135a6f0f011a5a378643b552
bewie03 <your-email@example.com>
2025-08-30 19:17:54 +1000

    feat: general enhancements

--- api/funding/index.ts
+++ api/funding/index.ts
@@ -3,7 +3,9 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
