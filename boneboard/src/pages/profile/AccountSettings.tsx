99fc3e6d0505da3564ce90e8841063f65717003c
bewie03 <your-email@example.com>
2025-09-04 15:40:31 +1000

    feat: general enhancements

--- boneboard/src/pages/profile/AccountSettings.tsx
+++ boneboard/src/pages/profile/AccountSettings.tsx
@@ -369,9 +369,7 @@ const AccountSettings: React.FC = () => {
               {/* Warning */}
               <div className="flex items-start space-x-3">
                 <div className="flex-shrink-0">
-                  <svg className="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-2.5L13.732 4c-.77-.833-1.964-.833-2.732 0L3.732 16c-.77.833.192 2.5 1.732 2.5z" />
-                  </svg>
                 </div>
                 <div>
                   <h3 className="text-lg font-medium text-red-900">
